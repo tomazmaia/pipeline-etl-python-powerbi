cbe9b8ce0e6cfbd2e02b07c23fd8c20bdcced3c2
tomaz <tomaz.maia131@gmail.com>
2026-02-20 12:05:20 -0300

    Primeiro commit: Pipeline completo

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pipeline de ETL: Vendas e Cotação de Moedas 
+
+Este projeto demonstra um pipeline de dados completo, desde a extração de dados brutos até a visualização em um Dashboard interativo.
+
+## Tecnologias Utilizadas
+* **Python 3.12+**: Processamento e limpeza de dados.
+* **Pandas**: Biblioteca principal para manipulação de DataFrames.
+* **Requests**: Consumo de API de câmbio em tempo real.
+* **Power BI**: Visualização de dados e criação de KPIs.
+
+## O que o projeto faz?
+1. **Extração:** Coleta dados de vendas locais (CSV) e cotação do dólar (API externa).
+2. **Transformação:** Limpa nomes de vendedores, trata tipos de dados (String para Float) e calcula comissões.
+3. **Carga:** Gera arquivos otimizados que alimentam um Dashboard automático no Power BI.
+
+## Como executar
+1. Execute `pipeline_csv.py` para processar as vendas.
+2. Execute `pipeline_cambio.py` para atualizar o câmbio.
+3. Abra os arquivos `PWBI_projectETL_example1.pbix e PWBI_projectETL_example2Cambio.pbix` no Power BI e clique em 'Atualizar'.
---
+++ pipeline_csv.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# ======== E - Extraction ===============
+df = pd.read_csv('vendas_brutas.csv', sep=',', encoding='utf-8-sig')
+
+# DEBUG colunas encontradas
+# print("Colunas encontradas: ", df.columns.tolist())
+
+# ======== T - Transformation ============
+# Limpar espaços e padronizar nomes (Capitalize)
+df.columns = df.columns.str.strip()
+
+if 'Vendedor' in df.columns:
+    df['Vendedor'] = df['Vendedor'].str.strip().str.title()
+
+    # Converter valor para número (trocar vírgula por ponto)
+    if not pd.api.types.is_numeric_dtype(df['Valor']):
+        print("Tratando coluna Valor: removendo virgulas e convertendo...")
+        df['Valor'] = df['Valor'].astype(str).str.replace(',','.')
+    # converter para float de forma segura
+    df['Valor'] = pd.to_numeric(df['Valor'], errors='coerce')
+
+    # Criar coluna de comissão (10%)
+    df['Comissao'] = df['Valor'] * 0.10
+    # ======= L - Load (Carga) ===============
+    df.to_csv('vendas_final.csv', index=False, decimal='.')
+    print("\n --- Visualização dos dados processados ---")
+    print(df.head())
+    print("ETL concluido: Arquivo 'vendas_final.csv' gerado com sucesso!")
+else:
+    print(f"Erro: A coluna 'Vendedor' não foi encontrada. As colunas disponíveis são: {df.columns.tolist()}")
